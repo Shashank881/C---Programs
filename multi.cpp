df347829701f25551b28afe9fd8fe1be9f7bc473
Shashank Lodia <lodiashashank@gmail.com>
2025-10-01 12:31:48 +0530

    Program for multiply

---
+++ multi.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter two values for multiply";
+    cin>>a>>b;
+    c=a*b;
+    cout<<"The result is "<<c;
+    
+}
